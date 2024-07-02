c4e76e755be3235d06d07e1e02eac45fe735210a
Bahar Abdi <bahar.abdi04@gmail.com>
2024-07-02 17:43:28 -0500

    Fix hover and pixelated trail across grid

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let size = 16;
 
-
-
 function createGrid() {
-    for (let i=0; i < (size*size); i++ ) {
+    for (let i=0; i < (size * size); i++ ) {
+        const container = document.querySelector(".container");
         let div = document.createElement("div");
-        container.appendChild("div");
-        div.style.cssText = "width: 2px; height: 2px;";
+        div.style.width = "calc(100% / 16)";
+        div.style.height = "calc(100% / 16)";
+        div.style.boxSizing = "border-box";
+        div.style.border = "1px solid #ddd";
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = getRandomColors();
+        });
+        container.appendChild(div);
     }
 }
+function getRandomColors() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+createGrid();
